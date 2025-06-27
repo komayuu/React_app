010d39c7dcb9465c70ade3139cf4e7eaa009efdc
小松優誠 <yuseikomatsu@MacBook-Pro.local>
2025-06-27 12:52:17 +0900

    レッスン04終了

--- lesson04/typescript/src/App.tsx
+++ lesson04/typescript/src/App.tsx
@@ -4,45 +4,50 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  // Person型を定義
-  type Person = {
+  // Product型を定義
+  type Product = {
     name: string,
-    city: string,
-    birthday: string,
+    category: string,
+    price: number,
   }
-  // 1人目のデータ
-  const person1:Person = {
-    name: "山田",
-    city: "東京都",
-    birthday: "2000年01月01日",
-  }
-  // 2人目のデータ
-  const person2:Person = {
-    name: "田中",
-    city: "大阪府",
-    birthday: "2002年02月02日",
-  }
+  const products:Array<Product> = [
+    {name: "TV", category: "家電", price: 100000},
+    {name: "Play Station 5", category: "ゲーム", price: 50000},
+    {name: "ノートパソコン", category: "家電", price: 200000},
+    {name: "Nintendo Switch", category: "ゲーム", price: 30000},
+  ];
 	
-  // 配列に２人分のデータを保存
-  const people:Array<Person> = [person1, person2];
-
   return (
     <>
-      <h1>名簿</h1>
       <table>
         <thead>
-          <th>名前</th>
-          <th>都道府県</th>
-          <th>生年月日</th>
+          <th>商品名</th>
+          <th>カテゴリー</th>
+          <th>価格</th>
+          <th>10万円以上？</th>
         </thead>
         <tbody>
-          {people.map((person, index) => (
-            <tr key={index}>
-              <td>{person.name}</td>
-              <td>{person.city}</td>
-              <td>{person.birthday}</td>
-            </tr>
-          ))}
+          {products.map((product, index) => {
+            if (product.price >= 100000) {
+              return (
+                <tr key={index}>
+                  <td>{product.name}</td>
+                  <td>{product.category}</td>
+                  <td>{product.price}</td>
+                  <td>YES</td>
+                </tr>
+              )
+            } else {
+              return (
+                <tr key={index}>
+                  <td>{product.name}</td>
+                  <td>{product.category}</td>
+                  <td>{product.price}</td>
+                  <td>NO</td>
+                </tr>
+              )
+            }
+          })}
         </tbody>
       </table>
     </>
