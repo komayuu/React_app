9ea1be6023a0811bc86bb0c1819de5f462e0c19d
小松優誠 <yuseikomatsu@MacBook-Pro.local>
2025-06-27 11:46:19 +0900

    レッスン03終了

--- lesson03/typescript-first/src/App.tsx
+++ lesson03/typescript-first/src/App.tsx
@@ -6,13 +6,20 @@ import './App.css'
 function App() {
 
   const fruits:Array<string> = ["りんご", "バナナ", "オレンジ"];
-
   fruits.push("ぶどう");
+
+  const newFruits = fruits.map((fruit, index) => {
+    return (
+      <li>
+        {index}番目に入っているのは{fruit}
+      </li>
+    );
+  });
 
   return (
     <>
       <ul>
-        フルーツは{fruits.length}個あります。
+        {newFruits}
       </ul>
     </>
   )
