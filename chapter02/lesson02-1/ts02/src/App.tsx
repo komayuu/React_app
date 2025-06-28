05581693e73df35aeec8a3e4fa3d7fca4b8e5843
小松優誠 <yuseikomatsu@MacBook-Pro.local>
2025-06-29 03:11:15 +0900

    レッスン02終了

--- chapter02/lesson02-1/ts02/src/App.tsx
+++ chapter02/lesson02-1/ts02/src/App.tsx
@@ -2,24 +2,24 @@ import './App.css'
 
 function App() {
 
-  // propsの型を定義
-  type ProfileProps = {
-    imageUrl: string,
-    name: string,
-    occupation: string,
-    hobbies: string,
+  // interfaceの場合
+  interface ProfileProps {
+    imageUrl?: string;
+    name: string;
+    occupation: string;
+    hobbies?: Array<string>;
   }
   
-  const ProfileCard = (props: ProfileProps) => {
-    const { imageUrl, name, occupation, hobbies } = props;
-    
+  // imageUrlにデフォルト値を設定
+  const ProfileCard = ({imageUrl = 'vite.svg', name, occupation, hobbies}: ProfileProps) => {
+
     return (
       <div className="card">
         <img src="vite.svg" alt="プロフィール画像" />
-        <h2 className='title'>{props.name}</h2>
+        <h2 className='title'>{name}</h2>
         <div>
-          <p>職業: {props.occupation}</p>
-          <p>趣味: {props.hobbies}</p>
+          <p>職業: {occupation}</p>
+          <p>趣味: {hobbies?.join(", ")}</p>
         </div>
         <p><button>Contact</button></p>
       </div>
@@ -28,9 +28,11 @@ function App() {
 
   return (
     <>
-      <ProfileCard imageUrl="vite.svg" name="山田 太郎" occupation="プロジェクトマネージャー" hobbies="プログラミング、読書"/>
-      <ProfileCard imageUrl="vite.svg" name="鈴木 花子" occupation="ソフトウェアエンジニア" hobbies="ピアノ、映画鑑賞" />
-      <ProfileCard imageUrl='vite.svg' name="佐藤 康太" occupation='インフラエンジニア' hobbies='ライブ鑑賞' />
+      <ProfileCard 
+        name="山田 太郎" 
+        occupation="プロジェクトマネージャー" 
+        hobbies={["プログラミング","読書"]}
+      />
     </>
   )
 }
