60cb8c08beb6c7bb7fe777d8b0f9029f28d8cb6d
小松優誠 <yuseikomatsu@MacBook-Pro.local>
2025-06-29 21:45:27 +0900

    三項演算子の場合

--- chapter03/lesson02/ts02/src/App.tsx
+++ chapter03/lesson02/ts02/src/App.tsx
@@ -5,21 +5,15 @@ function App() {
   const isEditable:boolean = true;
   const userName:string = "山田太郎";
 
-  if ( isEditable ) {
-    return (
-      <>
-        <p>お名前：</p>
-        <input type="text" placeholder={userName}/>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <p>お名前：</p>
-        <p>{userName}</p>
-      </>
-    );
-  }
+  const yellowText:boolean = true;
+
+  return (
+    <>
+      <p className={ yellowText ? "text-yellow" : ""}>お名前：</p>
+      { isEditable ? <input type="text" placeholder={userName}/> : <p>{userName}</p>}
+    </>
+  );
+
 }
 
 export default App
