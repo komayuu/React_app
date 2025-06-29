a6bdfd7f3885157a846f26fe68782ced3181235a
小松優誠 <yuseikomatsu@MacBook-Pro.local>
2025-06-29 19:37:59 +0900

    lesson04終了

--- chapter02/lesson04-2/ts05/src/App.tsx
+++ chapter02/lesson04-2/ts05/src/App.tsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Container, CssBaseline, Grid, Stack } from "@mui/material"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CssBaseline />
+
+      <Container maxWidth="lg">
+        <h1>メインコンテンツ</h1>
+        <p>ここにコンテンツが入ります。</p>
+      </Container>
+
+      <Container maxWidth="md">
+        <h1>メインコンテンツ</h1>
+        <p>ここにコンテンツが入ります。</p>
+      </Container>
+
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid item md={4} sx={{ backgroundColor: 'lightblue' }}>
+            <h1>4列分</h1>
+          </Grid>
+          <Grid item md={2} sx={{ backgroundColor: 'lightcoral' }}>
+            <h1>2列分</h1>
+          </Grid>
+          <Grid item md={1} sx={{ backgroundColor: 'lightcyan' }}>
+            <h2>1列分</h2>
+          </Grid>
+          <Grid item md={5} sx={{ backgroundColor: 'lightgreen' }}>
+            <h1>5列分</h1>
+          </Grid>
+        </Grid>
+      </Container>
+
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid item md={8} sx={{ backgroundColor: 'yellowgreen' }}>
+            <Stack spacing={2} direction="column">
+              <Button variant="contained">ボタン1</Button>
+              <Button variant="contained">ボタン2</Button>
+              <Button variant="contained">ボタン3</Button>
+            </Stack>
+          </Grid>
+          <Grid item md={4} sx={{ backgroundColor: 'violet' }}>
+            <Stack spacing={2} direction="row">
+              <Button variant="contained">ボタン1</Button>
+              <Button variant="contained">ボタン2</Button>
+              <Button variant="contained">ボタン3</Button>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   )
 }
